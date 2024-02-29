c6ffc9c8e28a7ebf47fd419ac6d27de7df5787df
Anthony.Elia <anthony.elia@libero.it>
2024-02-29 12:25:34 +0100

    lavarel 10

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreArticleRequest;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreArticlesRequest;
 
 class ArticleController extends Controller
 {
@@ -26,13 +26,51 @@ class ArticleController extends Controller
         return view('articoli.create');
     }
 
-    public function store(Request $request){
+    public function store(StoreArticlesRequest $request){
+
+        if($request->hasFile('image')){
+
+            if($request->file('image')->isValid()){
+
+                $request->file('image')->storeAs('public/articles', 'cover.jpg');
+
+            }else{
+                return 'Immagine non valida';
+            }
+        }else{
+            Article::create([
+                'title'=>$request->input('title'),
+               'content'=>$request->input('content')
+            ]);
+        }
+
+        
+
+            return redirect()->back()->with(['success'=>'Articolo salvato con successo']);
+
+        //$validator = Validator::make($request->all(),
+        //[
+        //    'title'=>'required|max:50',
+        //    'content'=> 'required|max:500'
+        //],
+        //[
+        //    'title.required'=>'il titolo non è corretto',
+        //    'title.max'=>'il titolo non è corretto',
+        //    'content.required'=>'il contenuto è richiesto',
+        //    'content.max'=>'il contenuto è troppo lungo'
+        //]
+        //);
+
+        //if($validator->fails()){
+        //    return redirect()->back()->withErrors($validator)->withInput;
+        //}else{
+        //    Article::create($request->all());
+        //}
 
         //Article::create([
         //    'title'=>$request->input('title'),
         //    'content'=>$request->input('content')
         //]);
 
-        Article::create($request->all());
     }
 }
